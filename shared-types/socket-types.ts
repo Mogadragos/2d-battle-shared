8f4dc1e0b9a3ec80c35a10ae05c016b18ee5855e
Mogador <jcaillaud17170@gmail.com>
2023-08-27 18:37:26 +0200

    Add soldier type to spawn event

--- shared-types/socket-types.ts
+++ shared-types/socket-types.ts
@@ -1,5 +1,6 @@
 import { Worker } from "worker_threads";
 import { GameData } from "./game/GameData";
+import { SoldierType } from "./game/SoldierType";
 
 export interface ServerToClientEvents {
     noArg: () => void;
@@ -16,7 +17,7 @@ export interface ServerToClientEvents {
 
 export interface ClientToServerEvents {
     hello: () => void;
-    spawn: () => void;
+    spawn: (type: SoldierType) => void;
 }
 
 export interface InterServerEvents {}
