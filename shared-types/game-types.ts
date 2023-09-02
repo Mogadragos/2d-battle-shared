691449b343ebededa8b5dc746462f72867a55ada
Mogador <jcaillaud17170@gmail.com>
2023-09-02 17:58:51 +0200

    export BuildEnum

--- shared-types/game-types.ts
+++ shared-types/game-types.ts
@@ -1,10 +1,11 @@
 import { AgeEnum, AgeConstants } from "./game/Age";
 import { AnimEnum } from "./game/Anim";
+import { BuildEnum } from "./game/Build";
 import { EntityEnum, EntityConstants, Entity } from "./game/Entity";
 import { Game } from "./game/Game";
 import { PlayerEnum, PlayerConstants, Player } from "./game/Player";
 
-export { AgeEnum, AnimEnum, EntityEnum, PlayerEnum };
+export { AgeEnum, AnimEnum, BuildEnum, EntityEnum, PlayerEnum };
 
 export type { AgeConstants, EntityConstants, PlayerConstants };
 
